2fa62176267890eacf6ad0819bf2e8ab477c3fbf
Doru Cioclea <doru.cioclea@corebuild.eu>
2017-09-29 14:06:53 +0300

    Added authentication project placeholder. This will contain the auth service(s) and dashboard

---
+++ src/Mvc.Server.Auth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mvc.Server.Auth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
