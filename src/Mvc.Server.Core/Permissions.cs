5cc41d264f7b0c25f539b908d83bc8eb22c9af22
cDoru <cioclea.doru@gmail.com>
2017-10-18 18:08:43 +0300

    Cleanup permissions - for now doing role authorization (at least until getting it all in a reasonably solid state)

--- src/Mvc.Server.Core/Permissions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Mvc.Server.Core
-{
-    public static class Permissions
-    {
-        public const string MessageAdminPermissionClaim = "message:admin";
-        public const string MessageUserPermissionClaim = "message:user";
-    }
-}
