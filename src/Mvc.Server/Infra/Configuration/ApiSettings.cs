1ac5bb7e0651f0896b5b3963c68d00184194eef0
cDoru <cioclea.doru@gmail.com>
2017-10-19 19:06:02 +0300

    make resharper happy again

--- src/Mvc.Server/Infra/Configuration/ApiSettings.cs
+++ src/Mvc.Server/Infra/Configuration/ApiSettings.cs
@@ -1,5 +1,9 @@
-﻿namespace Mvc.Server.DataObjects.Configuration
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Mvc.Server.DataObjects.Configuration
 {
+    [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
     public class ApiSettings
     {
         public string Audience { get; set; }
